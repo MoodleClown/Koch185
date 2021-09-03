955ba5ec105bafb9431b80c7e407751824803b99
MoodleClown <90038660+MoodleClown@users.noreply.github.com>
2021-09-03 14:16:44 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+	int height = 520;
+	int width = 840;
+	Mat img(height, width, CV_8UC3);
+
+	Point textOrg(100, img.rows / 2);
+	int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
+	double fontScale = 2;
+	Scalar color(200, 100, 50);
+	putText(img, "OpenCV Step By Step", textOrg, fontFace, fontScale, color);
+
+	//namedWindow("Hello world", 0);
+
+	imshow("Hello World", img);
+	
+	waitKey(0);
+	system("pause");
+	return 0;
+
+
+
+
+}
