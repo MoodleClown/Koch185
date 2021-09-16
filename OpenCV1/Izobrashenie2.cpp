5c9f5033e12398506f2c960d93bf4a2170c73e70
MoodleClown <90038660+MoodleClown@users.noreply.github.com>
2021-09-16 09:59:33 +0300

    Update Izobrashenie2.cpp

--- OpenCV1/Izobrashenie2.cpp
+++ OpenCV1/Izobrashenie2.cpp
@@ -17,12 +17,12 @@ using namespace std;
 Mat img;
 
 int main(int argc, char** argv) {
-	setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");//задает локаль, которая будет использоваться текущей программой.
 		char filename[80];
-		cout << "Введите имя файла, в который хотите внести изменения, и нажмите Enter" << endl;
-		cin.getline(filename, 80);
-		cout << "Открыть файл";
-		cout << filename << endl;
+		cout << "Введите имя файла, в который хотите внести изменения, и нажмите Enter" << endl;//стандартный выходной поток. Здесь мы выводим надпись в консоль
+		cin.getline(filename, 80);//предназначена для ввода данных из потока
+		cout << "Открыть файл";//выводим надпись
+		cout << filename << endl;// endl означает что значение начнется со следующей строки
 
 		/*int height = 520;
 		int width = 840;*/
@@ -32,20 +32,20 @@ int main(int argc, char** argv) {
 		
 	Mat src_gray;
 	Mat canny_output;
-	cvtColor(img, src_gray, COLOR_RGB2GRAY);
-	blur(src_gray, src_gray, Size(3, 3));
+	cvtColor(img, src_gray, COLOR_RGB2GRAY);//преобразует цветовое пространство матрицы
+	blur(src_gray, src_gray, Size(3, 3));//размытие 
 
 	/*double otsu_thresh_val = threshold(src_gray, img, 0, 255, THRESH_BINARY | THRESH_OTSU);
 	double high_thresh_val = otsu_thresh_val, lower_thresh_val = otsu_thresh_val * 0.5;
 	cout<<"Пороговое значение" << otsu_thresh_val;*/
-	double lower_thresh_val = 135;
-	double high_thresh_val = lower_thresh_val * 1.65;
-	Canny(src_gray, canny_output, lower_thresh_val, high_thresh_val, 3);
+	double lower_thresh_val = 135;//нижняя граница
+	double high_thresh_val = lower_thresh_val * 1.65;//врхняя граница
+	Canny(src_gray, canny_output, lower_thresh_val, high_thresh_val, 3);//Canny - детектор границ, а строчкой мы задаем ему "чувствительность", а точнее верхние и нижние границы
 
 
 	namedWindow("Серое изображение", WINDOW_AUTOSIZE);
 	imshow("Серое изображение", canny_output);
-	imwrite("canny_output.jpg", canny_output);
+	imwrite("canny_output.jpg", canny_output);//записывает индексированное изображение, находящееся в массиве X, и соответствующую ему цветовую палитру тар в файл filename
 
 		waitKey(0);
 		system("pause");
